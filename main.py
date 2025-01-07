255c08c2d330610ceae232226759e07a73aec77f
sagnikc395 <sagnikchatterjee395@gmail.com>
2025-01-07 14:25:33 +0530

    src: added time ticker

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from constants import *
 
 def main():
     pygame.init()
+    # restrict our game for 60 fps
+    clock = pygame.time.Clock()
+    dt = 0
+
     print("Starting asteroids!")
     # print(f"Screen width: {SCREEN_WIDTH}")
     # print(f"Screen height: {SCREEN_HEIGHT}")
@@ -16,6 +20,8 @@ def main():
         screen.fill("black")
         # flip updates the full display surface to the screen
         pygame.display.flip()
+        # updates the clock and convert from ms to seconds
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
