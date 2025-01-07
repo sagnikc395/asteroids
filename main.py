496a5cba5d296f38431d21c21e5cc1466469b402
sagnikc395 <sagnikchatterjee395@gmail.com>
2025-01-07 14:22:36 +0530

    src: close window

--- main.py
+++ main.py
@@ -10,7 +10,12 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
         screen.fill("black")
+        # flip updates the full display surface to the screen
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
